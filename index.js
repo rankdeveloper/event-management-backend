1691ae48ed808d5db5767900cef174a5d0e2e455
Rankush <iamrankush@gmail.com>
2025-01-15 22:26:29 +0530

    cors issue fix

--- index.js
+++ index.js
@@ -12,13 +12,21 @@ const cors = require("cors");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use((req, res, next) => {
+  const allowedOrigins = [
+    "http://localhost:5173",
+    "https://event-management-fullstack.netlify.app",
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.options("*", cors());
 
 app.use(express.json());
