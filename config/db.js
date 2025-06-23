7642d7bdfdf701c05c36415dc76e40850d6f1f70
Rankush <iamrankush@gmail.com>
2025-06-23 23:43:01 +0530

    variable changed

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = () => {
   mongoose
-    .connect(process.env.mongo_url)
+    .connect(process.env.MONGO_URI)
     .then(() => {
       console.log("Connected to MongoDB");
     })
